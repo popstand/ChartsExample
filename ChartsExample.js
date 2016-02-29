96c2f97d4f4bad25145394a97f064c9871763b0c
Kurt Braget <kurtbraget@gmail.com>
2016-02-28 17:34:34 -0800

    first commit

---
+++ ChartsExample.js
@@ -0,0 +1,61 @@
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  WebView
+} from 'react-native';
+
+import Dimensions from 'Dimensions';
+
+export default class ChartsExample extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <WebView style={styles.webview}
+                source={require('./www/amcharts-example.html')}
+              />
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  webview: {
+    flex: 1,
+    height: 200,
+    width: Dimensions.get('window').width
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
